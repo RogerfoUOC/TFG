7b84b7a313a8c6fd3eb43a16dd9be4ec3b2fb9e2
Roger Fogueras <rfogueras@uoc.edu>
2026-04-16 20:29:06 +0200

    estil botons canvi passa panell usuari

--- js/panell.js
+++ js/panell.js
@@ -3,6 +3,7 @@ const btnEditarmail     = document.getElementById("btn-editar-mail");
 const spanUserMail      = document.getElementById("span-user-mail");  
 const inputUserPass     = document.getElementById("input-user-pass");
 const btnGuardarMail    = document.getElementById("btn-guardar-mail");
+const btnCancelMail     = document.getElementById("btn-cancelar-mail");
 
 
 if (btnEditarmail) {
@@ -12,6 +13,7 @@ if (btnEditarmail) {
         spanUserMail.classList.add('ocult');
         inputUserPass.classList.remove('ocult');
         btnGuardarMail.classList.remove('ocult');
+        btnCancelMail.classList.remove('ocult');
     });
 }
 
@@ -21,6 +23,17 @@ if (btnGuardarMail) {
         btnEditarmail.classList.remove('ocult');
         inputUserPass.classList.add('ocult');
         btnGuardarMail.classList.add('ocult');
+        btnCancelMail.classList.add('ocult');
     });
 }   
+
+if (btnCancelMail) {
+    btnCancelMail.addEventListener('click', () => {
+        spanUserMail.classList.remove('ocult');
+        btnEditarmail.classList.remove('ocult');
+        btnCancelMail.classList.add('ocult');
+        inputUserPass.classList.add('ocult');
+        btnGuardarMail.classList.add('ocult');
+    });
+}  
 
