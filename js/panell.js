1791950d6b8e98b7c50a9d59a85ac85dd9c479a8
Roger Fogueras <rfogueras@uoc.edu>
2026-04-29 19:47:44 +0200

    blur al input mail del panell

--- js/panell.js
+++ js/panell.js
@@ -14,6 +14,109 @@ const passwordNouPanell     = document.getElementById("pass-nou");
 const passwordConfirmPanell = document.getElementById("pass-confirm");
 let emailOriginalPanell     = emailInputPanell.value;
 
+
+const validarEmailPanell = (email) => {
+    console.log("validarEmail");
+    //expressió regular que verifica si té el format de mail correcte
+    const emailFormat = /^[a-zA-Z0-9-_]{2,}@[a-zA-Z0-9-]{3,}\.[a-zA-Z]{2,}$/;
+    return emailFormat.test(email);
+};
+
+const validarPassBuitPanell = (camp, pass) => {
+    console.log(pass);
+    netejarError(camp);
+    if (pass.length <8) {
+        crearError(camp, 'La contrasenya ha de tenir com a mínim 8 caràcters');
+        console.log("valida NO");
+        camp.classList.add('input-error');
+        camp.classList.remove('input-correct');
+        return false;
+    }
+    camp.classList.remove('input-error');
+    camp.classList.add('input-correct');
+    console.log("valida OK");
+    return true;
+};
+
+
+const validarNouPassPanell = (camp, password) => {
+    netejarError(camp);
+    //expressió regular que verifica si té lletres, números, com a mínim un caràcter especial, i com a mínim 8 caràcters
+    const passwordFormat = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])[^\s]{8,}$/;
+    if (passwordFormat.test(password) == true) {
+        camp.classList.remove('input-error');
+        camp.classList.add('input-correct');
+        return true;
+    }
+    camp.classList.add('input-error');
+    camp.classList.remove('input-correct');
+    crearError(camp,'La contrasenya ha de tenir mínim 8 caràcters, lletres, números, mínim un caràcter especial i sense espais.');  
+    return false;
+};
+
+const confirmarPassPanell = (camp, password1, password2) => {
+    netejarError(camp);
+    console.log(password1);
+    console.log(password2); 
+    
+    if (password1 === password2) {
+        camp.classList.remove('input-error');
+        camp.classList.add('input-correct');
+        console.log("pass iguals");
+        return true;
+    } 
+    console.log("pass diferents");
+    camp.classList.add('input-error');
+    camp.classList.remove('input-correct');
+    crearError(camp,'Les contrasenyes no coincideixen.'); 
+    return false;
+}; 
+
+const netejarError = (camp) => {
+    const fila = camp.closest('.fila');
+    if (fila) {
+        fila.querySelector('.error-container').innerHTML = '';
+    } else {
+        const error = camp.closest('.grup-form')?.querySelector('.label-error');
+        if (error) error.remove();
+    }
+};
+
+//funció per crear els errors a cada input després de la validació
+const crearError = (camp, missatge) => {
+    netejarError(camp);
+    const errorElement = document.createElement('p');
+    errorElement.className = 'label-error';
+    errorElement.textContent = missatge;
+    
+    const fila = camp.closest('.fila');
+    if (fila) {
+        fila.querySelector('.error-container').appendChild(errorElement);
+    } else {
+        camp.insertAdjacentElement('afterend', errorElement);
+    }
+};
+
+
+
+emailInputPanell.addEventListener('focus', () => {
+    console.log("focus email panell");
+});
+
+emailInputPanell.addEventListener('blur', () => {
+    console.log("blur email panell");
+    if (emailInputPanell.classList.contains('ocult')) return; // no validem si està ocult
+    netejarError(emailInputPanell);
+    if (!validarEmailPanell(emailInputPanell.value)) {
+        emailInputPanell.classList.add('input-error');
+        emailInputPanell.classList.remove('input-correct');
+        crearError(emailInputPanell, 'El correu electrònic no té el format correcte, o està buit.');
+    } else {
+        emailInputPanell.classList.remove('input-error');
+        emailInputPanell.classList.add('input-correct');
+    }
+});
+
 btnEditarEmailPanell.addEventListener('click', () => {
     console.log("click editar mail");
     emailInputPanell.value = emailOriginalPanell;
@@ -116,105 +219,7 @@ function tancarmodalPassPanell() {
     });
     // botó cancel
     btnCancelPassPanell.addEventListener('click', tancarmodalPassPanell);
-
-emailInputPanell.addEventListener('blur', () => {
-    netejarError(emailInputPanell);
-    if (!validarEmailPanell(emailInputPanell.value)) {
-        emailInputPanell.classList.add('input-error');
-        crearError(emailInputPanell, 'El correu electrònic no té el format correcte, o està buit.');
-    } else {
-        emailInputPanell.classList.remove('input-error');
-        emailInputPanell.classList.add('input-correct');
-    }
-});
-
-
-const validarEmailPanell = (email) => {
-    console.log("validarEmail");
-    //expressió regular que verifica si té el format de mail correcte
-    const emailFormat = /^[a-zA-Z0-9-_]{2,}@[a-zA-Z0-9-]{3,}\.[a-zA-Z]{2,}$/;
-    return emailFormat.test(email);
-};
-console.log("validation.js carregat");
-
-const validarPassBuitPanell = (camp, pass) => {
-    console.log(pass);
-    netejarError(camp);
-    if (pass.length <8) {
-        crearError(camp, 'La contrasenya ha de tenir com a mínim 8 caràcters');
-        console.log("valida NO");
-        camp.classList.add('input-error');
-        camp.classList.remove('input-correct');
-        return false;
-    }
-    camp.classList.remove('input-error');
-    camp.classList.add('input-correct');
-    console.log("valida OK");
-    return true;
-};
-
-
-const validarNouPassPanell = (camp, password) => {
-    netejarError(camp);
-    //expressió regular que verifica si té lletres, números, com a mínim un caràcter especial, i com a mínim 8 caràcters
-    const passwordFormat = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])[^\s]{8,}$/;
-    if (passwordFormat.test(password) == true) {
-        camp.classList.remove('input-error');
-        camp.classList.add('input-correct');
-        return true;
-    }
-    camp.classList.add('input-error');
-    camp.classList.remove('input-correct');
-    crearError(camp,'La contrasenya ha de tenir mínim 8 caràcters, lletres, números, mínim un caràcter especial i sense espais.');  
-    return false;
-};
-
-const confirmarPassPanell = (camp, password1, password2) => {
-    netejarError(camp);
-    console.log(password1);
-    console.log(password2); 
-
-    if (password1 === password2) {
-        camp.classList.remove('input-error');
-        camp.classList.add('input-correct');
-        console.log("pass iguals");
-        return true;
-    } 
-    console.log("pass diferents");
-    camp.classList.add('input-error');
-    camp.classList.remove('input-correct');
-    crearError(camp,'Les contrasenyes no coincideixen.'); 
-    return false;
-}; 
-
-
-const netejarError = (camp) => {
-    const fila = camp.closest('.fila');
-    if (fila) {
-        fila.querySelector('.error-container').innerHTML = '';
-    } else {
-        const error = camp.closest('.grup-form')?.querySelector('.label-error');
-        if (error) error.remove();
-    }
-};
-
-//funció per crear els errors a cada input després de la validació
-const crearError = (camp, missatge) => {
-    netejarError(camp);
-    const errorElement = document.createElement('p');
-    errorElement.className = 'label-error';
-    errorElement.textContent = missatge;
     
-    const fila = camp.closest('.fila');
-    if (fila) {
-        fila.querySelector('.error-container').appendChild(errorElement);
-    } else {
-        camp.insertAdjacentElement('afterend', errorElement);
-    }
-};
-
-
-
 passActualPanell.addEventListener('blur', () => {
     netejarError(passActualPanell);
     validarPassBuitPanell(passActualPanell, passActualPanell.value);
@@ -251,41 +256,41 @@ passActualPanell.addEventListener('blur', () => {
 });
 
 passwordNouPanell.addEventListener('blur', () => {
-        netejarError(passwordNouPanell);
-        validarNouPassPanell(passwordNouPanell, passwordNouPanell.value);
-        if (passwordNouPanell.classList.contains('input-correct') && passActualPanell.value !== '') {
-            if (passActualPanell.value === passwordNouPanell.value) {
-                passwordNouPanell.classList.remove('input-correct');
-                passwordNouPanell.classList.add('input-error');
-                crearError(passwordNouPanell, 'La nova contrasenya és igual que l\'actual.');
-                // validacio de pass confrm 
-                if (passwordConfirmPanell.value !== '') {
-                    passwordConfirmPanell.classList.remove('input-correct');
-                    passwordConfirmPanell.classList.add('input-error');
-                    netejarError(passwordConfirmPanell);
-                    crearError(passwordConfirmPanell, 'Primer has d\'introduir una contrasenya vàlida.');
-                }
-                return;
-            }
-        }                                                                   
-        if (passwordConfirmPanell.value !== '' || passwordConfirmPanell.classList.contains('input-error')) {
-            netejarError(passwordConfirmPanell);
-            if (passwordNouPanell.classList.contains('input-correct')) {
-                confirmarPassPanell(passwordConfirmPanell, passwordNouPanell.value, passwordConfirmPanell.value);            } else {
+    netejarError(passwordNouPanell);
+    validarNouPassPanell(passwordNouPanell, passwordNouPanell.value);
+    if (passwordNouPanell.classList.contains('input-correct') && passActualPanell.value !== '') {
+        if (passActualPanell.value === passwordNouPanell.value) {
+            passwordNouPanell.classList.remove('input-correct');
+            passwordNouPanell.classList.add('input-error');
+            crearError(passwordNouPanell, 'La nova contrasenya és igual que l\'actual.');
+            // validacio de pass confrm 
+            if (passwordConfirmPanell.value !== '') {
                 passwordConfirmPanell.classList.remove('input-correct');
                 passwordConfirmPanell.classList.add('input-error');
+                netejarError(passwordConfirmPanell);
                 crearError(passwordConfirmPanell, 'Primer has d\'introduir una contrasenya vàlida.');
             }
+            return;
         }
-    });
-
-    passwordConfirmPanell.addEventListener('blur', () => {
+    }                                                                   
+    if (passwordConfirmPanell.value !== '' || passwordConfirmPanell.classList.contains('input-error')) {
         netejarError(passwordConfirmPanell);
         if (passwordNouPanell.classList.contains('input-correct')) {
-            confirmarPassPanell(passwordConfirmPanell, passwordNouPanell.value, passwordConfirmPanell.value);
-        } else {
+            confirmarPassPanell(passwordConfirmPanell, passwordNouPanell.value, passwordConfirmPanell.value);            } else {
             passwordConfirmPanell.classList.remove('input-correct');
             passwordConfirmPanell.classList.add('input-error');
             crearError(passwordConfirmPanell, 'Primer has d\'introduir una contrasenya vàlida.');
         }
-    });
+    }
+});
+
+passwordConfirmPanell.addEventListener('blur', () => {
+    netejarError(passwordConfirmPanell);
+    if (passwordNouPanell.classList.contains('input-correct')) {
+        confirmarPassPanell(passwordConfirmPanell, passwordNouPanell.value, passwordConfirmPanell.value);
+    } else {
+        passwordConfirmPanell.classList.remove('input-correct');
+        passwordConfirmPanell.classList.add('input-error');
+        crearError(passwordConfirmPanell, 'Primer has d\'introduir una contrasenya vàlida.');
+    }
+});
